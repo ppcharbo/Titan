300c54a3c08656dd3a31586ba0e873779b8aac2e
PierrePaul <pierrepaul.charbonnier@gmail.com>
2021-03-15 17:58:59 +0100

    fixed planet visualization

--- src/titan/PlanetScreen.java
+++ src/titan/PlanetScreen.java
@@ -1,42 +1,21 @@
 package titan;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GridBagLayout;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class PlanetScreen {
-	
+
 	public PlanetScreen() {
-		JFrame planetFrame  = new JFrame();
+		JFrame planetFrame = new JFrame();
 		planetFrame.setSize(1600, 1600);
 		planetFrame.setTitle("Journey to Titan");
 		planetFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	    JPanel panel = new JPanel(); //new BorderLayout()
-	    //panel.setBackground(new Color(0, 0, 0));
-	    
-	    //xLT, yLT, width, height, RGB code
-	    PlanetComponent sun = new PlanetComponent(50, 450, 100, 100, 255, 165, 0);
-	    PlanetComponent mercury = new PlanetComponent(150, 450, 50, 50, 169, 169, 169);
-	    
-	    //panel.add(sun);
-		//panel.add(mercury);
-	    sun.setVisible(true);
-	    //push
-	    mercury.setVisible(true);
-	    planetFrame.add(sun);
-	    planetFrame.add(mercury);
-	    //panel.setVisible(true);
-	    //planetFrame.add(panel);
-	    
-	    planetFrame.setVisible(true);
-	    
+		SolarSystem solarSystem = new SolarSystem();
+		solarSystem.addPlanet(50, 450, 100, 100, 255, 165, 0);
+		solarSystem.addPlanet(150, 450, 50, 50, 169, 169, 169);
+		planetFrame.getContentPane().add(solarSystem);
+		planetFrame.setVisible(true);
+
 	}
 
-	
-    
-    
 }
---
+++ src/titan/SolarSystem.java
@@ -0,0 +1,31 @@
+package titan;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+
+import javax.swing.JComponent;
+//import titan.Planet;
+
+public class SolarSystem extends JComponent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static ArrayList<Planet> solarSystem=new ArrayList<>();
+	
+	public void paintComponent(Graphics g1) {
+		Graphics2D g2 = (Graphics2D) g1;
+	
+		for (Planet planet : solarSystem) {
+			planet.draw(g2);	
+		}
+		
+	}
+	public void addPlanet(int x, int y, int width, int height, int r, int g, int b) {
+		Planet planet = new Planet(x, y, width, height, r, g, b);
+		solarSystem.add(planet);
+		
+	}
+}
