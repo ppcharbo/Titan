1610a2300263bf4f330097229830ee26dafefb51
TheChickenAgent <m.elands@student.maastrichtuniversity.nl>
2021-03-15 16:31:25 +0100

    GUI work in progress
    Removed Sun classes, improved PlanetComponent and PlanetScreen

--- src/titan/GUIMain.java
+++ src/titan/GUIMain.java
@@ -1,11 +1,8 @@
 package titan;
 
 public class GUIMain extends GUI {
-	
-	public static void main(String[] args) {
-		
-		GUI myGUI = new GUI();
-		
-	}
 
+	public static void main(String[] args) {
+		GUI myGUI = new GUI();
+	}
 }
--- src/titan/PlanetComponent.java
+++ src/titan/PlanetComponent.java
@@ -3,14 +3,31 @@ package titan;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JComponent;
+//import titan.Planet;
 
-import titan.Planet;
+public class PlanetComponent extends JComponent {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
 	
-	public class PlanetComponent extends JComponent {
-			
-			public void paintComponent (Graphics g) {
-	        Graphics2D g2 = (Graphics2D) g;
-	        Planet planet = new Planet(800, 600);
-	        planet.draw(g2);
-	    }
+	private int r;
+	private int g;
+	private int b;
+	
+	public PlanetComponent(int x, int y, int width, int height, int r, int g, int b) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		
+		this.r = r;
+		this.g = g;
+		this.b = b;
 	}
+	public void paintComponent(Graphics g1) {
+		Graphics2D g2 = (Graphics2D) g1;
+		Planet planet = new Planet(x, y, width, height, r, g, b);
+		planet.draw(g2);
+	}
+}
