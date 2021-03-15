701dae33797ce53206e077eda4a2a46de2d3b70a
TheChickenAgent <m.elands@student.maastrichtuniversity.nl>
2021-03-15 16:46:57 +0100

    GUI 2.0
    Work in progress

--- src/titan/PlanetScreen.java
+++ src/titan/PlanetScreen.java
@@ -15,21 +15,21 @@ public class PlanetScreen {
 		planetFrame.setTitle("Journey to Titan");
 		planetFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	    //JPanel panel = new JPanel(); //new BorderLayout()
+	    JPanel panel = new JPanel(); //new BorderLayout()
 	    //panel.setBackground(new Color(0, 0, 0));
 	    
 	    //xLT, yLT, width, height, RGB code
-	    PlanetComponent sun = new PlanetComponent(0, 450, 100, 100, 255, 165, 0);
+	    PlanetComponent sun = new PlanetComponent(50, 450, 100, 100, 255, 165, 0);
 	    PlanetComponent mercury = new PlanetComponent(150, 450, 50, 50, 169, 169, 169);
 	    
-	    //panel.add(sun);  
+	    //panel.add(sun);
 		//panel.add(mercury);
+	    sun.setVisible(true);
+	    mercury.setVisible(true);
 	    planetFrame.add(sun);
 	    planetFrame.add(mercury);
 	    //panel.setVisible(true);
 	    //planetFrame.add(panel);
-	    
-	    
 	    
 	    planetFrame.setVisible(true);
 	    
