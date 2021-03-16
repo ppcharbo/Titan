62b5022916c69513642ced4c181b643a4793fd22
Shirley De Koster <shirleydekoster02@gmail.com>
2021-03-16 18:03:11 +0100

    planet position correction

--- src/titan/SystemPlanet.java
+++ src/titan/SystemPlanet.java
@@ -19,22 +19,22 @@ public class SystemPlanet {
 	public Vector3dInterface earthActualPosition = new Vector3d(-1.471922101663588e+11, -2.860995816266412e+10, 8.278183193596080e+06);
 	public Vector3dInterface mercuryActualPosition = new Vector3d(6.047855986424127e+06, -6.801800047868888e+10, -5.702742359714534e+09);
 	public Vector3dInterface mercuryInitialPosition = new Vector3d(-1.471922101663588e+11, -2.860995816266412e+10, 8.278183193596080e+06);
-	public Vector3dInterface VenusActualPosition = new Vector3d(-9.435345478592035e+10, 5.350359551033670e+10, 6.131453014410347e+09);
-	public Vector3dInterface VenusInitialPosition = new Vector3d(-9.435345478592035e+10, 5.350359551033670e+10, 6.131453014410347e+09);
-	public Vector3dInterface MoonInitialPosition = new Vector3d(-1.472343904597218e+11, -2.822578361503422e+10, 1.052790970065631e+07);
-	public Vector3dInterface MoonActualPosition = new Vector3d(-1.472343904597218e+11, -2.822578361503422e+10, 1.052790970065631e+07);
-	public Vector3dInterface MarsInitialPosition = new Vector3d(-3.615638921529161e+10, -2.167633037046744e+11, -3.687670305939779e+09);
-	public Vector3dInterface MarsActualPosition = new Vector3d(-3.615638921529161e+10, -2.167633037046744e+11, -3.687670305939779e+09);
-	public Vector3dInterface JupiterInitialPosition = new Vector3d(1.781303138592153e+11, -7.551118436250277e+11, -8.532838524802327e+08);
-	public Vector3dInterface JupiterActualPosition = new Vector3d(1.781303138592153e+11, -7.551118436250277e+11, -8.532838524802327e+08);
-	public Vector3dInterface SaturnInitialPosition = new Vector3d(6.328646641500651e+11, -1.358172804527507e+12, -1.578520137930810e+09);
-	public Vector3dInterface SaturnActualPosition = new Vector3d(6.328646641500651e+11, -1.358172804527507e+12, -1.578520137930810e+09);
-	public Vector3dInterface TitanInitialPosition = new Vector3d(6.332873118527889e+11, -1.357175556995868e+12, -2.134637041453660e+09);
-	public Vector3dInterface TitanActualPosition = new Vector3d(6.332873118527889e+11, -1.357175556995868e+12, -2.134637041453660e+09);
-	public Vector3dInterface UranusInitialPosition = new Vector3d(2.395195786685187e+12, 1.744450959214586e+12, -2.455116324031639e+10);
-	public Vector3dInterface UranusActualPosition = new Vector3d(2.395195786685187e+12, 1.744450959214586e+12, -2.455116324031639e+10);
-	public Vector3dInterface NeptuneInitialPosition = new Vector3d(4.382692942729203e+12, -9.093501655486243e+11, -8.227728929479486e+10);
-	public Vector3dInterface NeptuneActualPosition = new Vector3d(4.382692942729203e+12, -9.093501655486243e+11, -8.227728929479486e+10);
+	public Vector3dInterface venusActualPosition = new Vector3d(-9.435345478592035e+10, 5.350359551033670e+10, 6.131453014410347e+09);
+	public Vector3dInterface venusInitialPosition = new Vector3d(-9.435345478592035e+10, 5.350359551033670e+10, 6.131453014410347e+09);
+	public Vector3dInterface moonInitialPosition = new Vector3d(-1.472343904597218e+11, -2.822578361503422e+10, 1.052790970065631e+07);
+	public Vector3dInterface moonActualPosition = new Vector3d(-1.472343904597218e+11, -2.822578361503422e+10, 1.052790970065631e+07);
+	public Vector3dInterface marsInitialPosition = new Vector3d(-3.615638921529161e+10, -2.167633037046744e+11, -3.687670305939779e+09);
+	public Vector3dInterface marsActualPosition = new Vector3d(-3.615638921529161e+10, -2.167633037046744e+11, -3.687670305939779e+09);
+	public Vector3dInterface jupiterInitialPosition = new Vector3d(1.781303138592153e+11, -7.551118436250277e+11, -8.532838524802327e+08);
+	public Vector3dInterface jupiterActualPosition = new Vector3d(1.781303138592153e+11, -7.551118436250277e+11, -8.532838524802327e+08);
+	public Vector3dInterface saturnInitialPosition = new Vector3d(6.328646641500651e+11, -1.358172804527507e+12, -1.578520137930810e+09);
+	public Vector3dInterface saturnActualPosition = new Vector3d(6.328646641500651e+11, -1.358172804527507e+12, -1.578520137930810e+09);
+	public Vector3dInterface titanInitialPosition = new Vector3d(6.332873118527889e+11, -1.357175556995868e+12, -2.134637041453660e+09);
+	public Vector3dInterface titanActualPosition = new Vector3d(6.332873118527889e+11, -1.357175556995868e+12, -2.134637041453660e+09);
+	public Vector3dInterface uranusInitialPosition = new Vector3d(2.395195786685187e+12, 1.744450959214586e+12, -2.455116324031639e+10);
+	public Vector3dInterface uranusActualPosition = new Vector3d(2.395195786685187e+12, 1.744450959214586e+12, -2.455116324031639e+10);
+	public Vector3dInterface neptuneInitialPosition = new Vector3d(4.382692942729203e+12, -9.093501655486243e+11, -8.227728929479486e+10);
+	public Vector3dInterface neptuneActualPosition = new Vector3d(4.382692942729203e+12, -9.093501655486243e+11, -8.227728929479486e+10);
 
 	public Vector3dInterface[] trajectoryGeneric(Vector3dInterface p0, Vector3dInterface v0, double tf, double h, Vector3dInterface centerposition, Vector3dInterface positionMoving, double massCenter, double massMoving) {
 		double time = 0;
@@ -67,7 +67,7 @@ public class SystemPlanet {
 
 	public void changeTime(double tf, double h) {
 
-		//trajectoryGeneric(earthInitialPosition, earthActualPosition, tf, h, sunPosition, mercuryPosition, tf, h);
+		trajectoryGeneric(earthInitialPosition, earthActualPosition, tf, h, sunPosition, mercuryActualPosition, tf, h);
 
 	}
 
