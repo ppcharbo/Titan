d8a614aef1a9838f6c920f75cd8401b6c443ebb2
basaratojo <pierrepaul.charbonnier@gmail.com>
2021-03-16 11:33:31 +0100

    probe

---
+++ src/titan/ProbeSimulator.java
@@ -0,0 +1,17 @@
+package titan;
+
+public class ProbeSimulator implements ProbeSimulatorInterface {
+
+	@Override
+	public Vector3dInterface[] trajectory(Vector3dInterface p0, Vector3dInterface v0, double[] ts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector3dInterface[] trajectory(Vector3dInterface p0, Vector3dInterface v0, double tf, double h) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
