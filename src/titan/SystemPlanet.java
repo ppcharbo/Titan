cf89070cae16fa00aef9cbe901b0893b90966a24
Shirley De Koster <shirleydekoster02@gmail.com>
2021-03-16 17:45:13 +0100

    mass declaration

--- src/titan/SystemPlanet.java
+++ src/titan/SystemPlanet.java
@@ -2,9 +2,17 @@ package titan;
 
 public class SystemPlanet {
 	public double G = 6.66667 * Math.pow(10, -11);// Gravity universally 
-	public double earthMass = 5.97219 * Math.pow(10, 24);
-	public double solarMass = 1.988500 * Math.pow(10, 30);
+	public double earthMass = 5.97219e24;
+	public double solarMass = 1.988500e30;
 	public double mercuryMass = 3.302e23;
+	public double venusMass = 4.8685e24;
+	public double moonMass = 7.349e22;
+	public double marsMass = 6.4171e23;
+	public double jupiterMass = 1.89813e27;
+	public double saturnMass = 5.6834e26;
+	public double titanMass = 1.34553e23;
+	public double uranusMass = 8.6813e25;
+	public double neptuneMass = 1.02413e26;
 
 	public Vector3dInterface sunPosition = new Vector3d(-6.806783239281648e+08, 1.080005533878725e+09, 6.564012751690170e+06);
 	public Vector3dInterface earthInitialPosition = new Vector3d(-1.471922101663588e+11, -2.860995816266412e+10, 8.278183193596080e+06);
