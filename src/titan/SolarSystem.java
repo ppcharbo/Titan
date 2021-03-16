ead6a60b45fd6c666d270e1a7305b32b57a3cdff
Shirley De Koster <shirleydekoster02@gmail.com>
2021-03-16 10:10:16 +0100

    background

--- src/titan/SolarSystem.java
+++ src/titan/SolarSystem.java
@@ -1,11 +1,18 @@
 package titan;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 //import titan.Planet;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class SolarSystem extends JComponent {
 
@@ -17,6 +24,7 @@ public class SolarSystem extends JComponent {
 	
 	public void paintComponent(Graphics g1) {
 		Graphics2D g2 = (Graphics2D) g1;
+		 
 	
 		for (Planet planet : solarSystem) {
 			planet.draw(g2);	
