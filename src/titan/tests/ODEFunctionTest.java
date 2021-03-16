34d5efddfc512a36840643fb3d2e46ee13aad25b
basaratojo <pierrepaul.charbonnier@gmail.com>
2021-03-16 10:52:02 +0100

    test case of function

---
+++ src/titan/tests/ODEFunctionTest.java
@@ -0,0 +1,14 @@
+package titan.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ODEFunctionTest {
+
+	@Test
+	public void testCall() {
+		fail("Not yet implemented");
+	}
+
+}
