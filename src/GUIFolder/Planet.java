65e4f995dc06c04cdcb46d99668c7ed2a4287871
Basaratojo <chapat@gmail.com>
2021-03-17 19:02:32 +0100

    changed to arrayList

--- src/GUIFolder/Planet.java
+++ src/GUIFolder/Planet.java
@@ -3,91 +3,108 @@ package GUIFolder;
 import java.awt.*;
 import java.util.*;
 
-public class Planet
-{
+import javax.swing.JPanel;
+
+public class Planet {
 
 	private int mass = 0;
 	private int diameter = 0;
-    private double xLoc = 0; 
-    private double yLoc = 0;
-    private double velX = 0; 
-    private double velY = 0;
-    //private double speed = 0;
-    Color color;
-    private double acceleration =0;
-    private double dirX = 0;
-    private double dirY = 0;
-    private double distance = 0;
-    private double initial=1000;
-    private double max=0;
-    boolean visible;
-    /**
-     * Constructor for objects of class Planet
-     */
-    public Planet(int r, int g, int b, double xCoordinate, double yCoordinate, int diameter, double vx, double vy, int mass){
+	private double xLoc = 0;
+	private double yLoc = 0;
+	private double velX = 0;
+	private double velY = 0;
+	//private double speed = 0;
+	Color color;
+	private double acceleration = 0;
+	private double dirX = 0;
+	private double dirY = 0;
+	private double distance = 0;
+	private double initial = 1000;
+	private double max = 0;
+	boolean visible;
+	private String label;
+	private JPanel parent;
 
-        color = new Color(r, g, b);
-        xLoc = xCoordinate;
-        yLoc = yCoordinate;
-        this.diameter = diameter;
-        velX = vx;
-        velY = vy;
-        this.mass = mass;
+	public Planet(JPanel parent, String label, int r, int g, int b, double xCoordinate, double yCoordinate, int diameter, double vx, double vy, int mass) {
 
+		this.parent = parent;
+		this.label = label;
+		color = new Color(r, g, b);
+		xLoc = xCoordinate;
+		yLoc = yCoordinate;
+		this.diameter = diameter;
+		velX = vx;
+		velY = vy;
+		this.mass = mass;
 
-    }
-    /*
-    public Planet(double x, double y, double xVelocity, double yVelocity, int bodyMass, int bodyDiameter, Color bodyColor)
-    {
-       xLoc = x;
-       yLoc = y;
-       velX = xVelocity;
-       velY = yVelocity;
-       	mass = bodyMass;
-        diameter = bodyDiameter;
-       color = bodyColor;
-    }
-    */
-    public double getXPosition(){
-    	return xLoc;
-    	}
-    public double getYPosition(){
-    	return yLoc;
-    	}
-    public int getMass(){
-    	return mass;
-    	}
-    public int getDiameter(){
-    	return diameter;
-    	}
-    public void move()
-    {
-        xLoc += velX;
-        yLoc += velY;
-    }
+	}
+
+	/*
+	public Planet(double x, double y, double xVelocity, double yVelocity, int bodyMass, int bodyDiameter, Color bodyColor)
+	{
+	   xLoc = x;
+	   yLoc = y;
+	   velX = xVelocity;
+	   velY = yVelocity;
+	   	mass = bodyMass;
+	    diameter = bodyDiameter;
+	   color = bodyColor;
+	}
+	*/
+	public double getXPosition() {
+		return xLoc;
+	}
+
+	public double getYPosition() {
+		return yLoc;
+	}
+
+	public int getMass() {
+		return mass;
+	}
+
+	public int getDiameter() {
+		return diameter;
+	}
+
+	public void move() {
+		xLoc += velX;
+		yLoc += velY;
+	}
+
+	public void update(double StarX, double StarY, int StarMass) {
+		distance = Math.sqrt((StarX - xLoc) * (StarX - xLoc) + (StarY - yLoc) * (StarY - yLoc));
+		initial = Math.min(distance, initial);
+		max = Math.max(distance, max);
+
+		acceleration = StarMass / distance / distance;
+
+		dirX = (StarX - xLoc) / distance;
+		dirY = (StarY - yLoc) / distance;
+
+		velX += dirX * acceleration;
+		velY += dirY * acceleration;
+		move();
+
+	}
+
+	@Deprecated
+	public void draw(Graphics g, double size) {
+		g.setColor(color);
+
+		g.fillOval((int) (650 + (xLoc - diameter / 2 - 650) * size), (int) (500 + (yLoc - diameter / 2 - 500) * size), (int) (diameter * size), (int) (diameter * size));
+	}
+
+	public void draw(Graphics g, double size, int windth, int height) {
+		g.setColor(color);
  
-    public void update(double StarX, double StarY, int StarMass)
-    {
-        distance = Math.sqrt((StarX - xLoc)*(StarX - xLoc) + (StarY - yLoc)*(StarY - yLoc));
-        initial = Math.min(distance,initial);
-        max = Math.max(distance,max);
-        
-        acceleration = StarMass/distance/distance;
-        
-        dirX = (StarX-xLoc)/distance;
-        dirY = (StarY-yLoc)/distance;
-        
-        velX += dirX * acceleration;
-        velY += dirY * acceleration;
-        move();
-       
-    }
-    public void draw(Graphics g, double size)
-    {
-        g.setColor(color);
-        g.fillOval((int)(650+(xLoc-diameter/2-650)*size), (int)(500+(yLoc-diameter/2-500)*size),
-                    (int)(diameter*size), (int)(diameter*size));
-    }
+			int x = (int) (windth + (xLoc - diameter / 2 - windth) * size);
+			int y = (int) (height + (yLoc - diameter / 2 - height) * size);
+
+			System.out.println(" x = " + x + " y= " + y + " windth =" + windth + " height =" + height);
+			g.fillOval(x, y, (int) (diameter * size), (int) (diameter * size));
+	
+		
+	}
+
 }
-
-
--- src/GUIFolder/SystemPlanet.java
+++ src/GUIFolder/SystemPlanet.java
@@ -3,12 +3,13 @@ package GUIFolder;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class SystemPlanet extends JPanel {
-	Planet[] celestialBodies = new Planet[9];
+	ArrayList<Planet> celestialBodies = new ArrayList<Planet>();
 	private Image img;
 
 	final static int DELAY = 5;
@@ -22,15 +23,15 @@ public class SystemPlanet extends JPanel {
 
 		img = icon.getImage();
 
-		celestialBodies[0] = new Planet(128, 128, 128, 600, 450, 8, -4.7, 0, 9);
-		celestialBodies[1] = new Planet(207, 153, 52, 752, 400, 12, 0, 2.5, 900);
-		celestialBodies[2] = new Planet(0, 0, 255, 600, 150, 11, 1.8, 0, 900);
-		celestialBodies[3] = new Planet(255, 0, 0, 650, -50, 7, 1.2, 0, 900);
-		celestialBodies[4] = new Planet(255, 140, 0, 600, -100, 20, 1.2, 0, 900);
-		celestialBodies[5] = new Planet(112, 128, 144, 600, -150, 15, 1.2, 0, 900);
-		celestialBodies[6] = new Planet(196, 233, 238, 600, -175, 15, 1.2, 0, 900);
-		celestialBodies[7] = new Planet(66, 98, 243, 0, 400, 13, 0, -1.2, 900);
-		celestialBodies[8] = new Planet(255, 140, 0, 600, 400, 30, .1, 0, 1000);
+		celestialBodies.add(new Planet(this,"",128, 128, 128, 600, 450, 8, -4.7, 0, 9));
+		celestialBodies.add(new Planet(this,"",207, 153, 52, 752, 400, 12, 0, 2.5, 900));
+		celestialBodies.add(new Planet(this,"",0, 0, 255, 600, 150, 11, 1.8, 0, 900));
+		celestialBodies.add(new Planet(this,"",255, 0, 0, 650, -50, 7, 1.2, 0, 900));
+		celestialBodies.add(new Planet(this,"",255, 140, 0, 600, -100, 20, 1.2, 0, 900));
+		celestialBodies.add(new Planet(this,"",112, 128, 144, 600, -150, 15, 1.2, 0, 900));
+		celestialBodies.add(new Planet(this,"",196, 233, 238, 600, -175, 15, 1.2, 0, 900));
+		celestialBodies.add(new Planet(this,"",66, 98, 243, 0, 400, 13, 0, -1.2, 900));
+		celestialBodies.add(new Planet(this,"sun",255, 140, 0, 600, 400, 30, .1, 0, 1000));
 
 		//setBackground(Color.BLACK);
 
@@ -50,20 +51,31 @@ public class SystemPlanet extends JPanel {
 		Graphics2D g2 = (Graphics2D) g;
 		int width2 = getWidth();
 		int height2 = getHeight();
-		System.out.println("width  " + width2 + " height  " + height2);
-		 
+	//	System.out.println("width  " + width2 + " height  " + height2);
+
 		g2.drawImage(img, 0, 0, width2, height2, this);
 
 		for (Planet body : celestialBodies)
-			body.draw(g, size);
+		//	body.draw(g, size);
+			body.draw(g, size,getWidth(),getHeight());
 	}
 
 	private void gameLoop() {
 
 		while (true) {
 			if (!stop) {
+				// original
+				/*
 				for (int i = 0; i < celestialBodies.length - 1; i++) {
 					celestialBodies[i].update(celestialBodies[8].getXPosition(), celestialBodies[8].getYPosition(), celestialBodies[8].getMass());
+				}
+				*/
+				// this is the SUN 
+				//we must update relative to the sun position
+				Planet sun = celestialBodies.get(celestialBodies.size() - 1);
+				for (Planet planet : celestialBodies) {
+					if(planet!=sun)
+					planet.update(sun.getXPosition(), sun.getYPosition(), sun.getMass());
 				}
 			}
 			repaint();
