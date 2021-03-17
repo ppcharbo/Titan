5b692761a88e73e58de11144cefd2ce31e62b542
Basaratojo <chapat@gmail.com>
2021-03-17 16:05:47 +0100

    fixed resize

--- src/GUIFolder/SystemPlanet.java
+++ src/GUIFolder/SystemPlanet.java
@@ -1,11 +1,13 @@
 package GUIFolder;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
 public class SystemPlanet extends JPanel {
-	Model model;
 	Planet[] celestialBodies = new Planet[9];
 	private Image img;
 
@@ -19,10 +21,6 @@ public class SystemPlanet extends JPanel {
 		ImageIcon icon = new ImageIcon(this.getClass().getResource("background.jpg")); //https://www.pexels.com/photo/starry-sky-998641/
 
 		img = icon.getImage();
-		// extract the image out of it
-		model = new Model();
-		model.setPreferredSize(new Dimension(1200, 1200));
-		add(model);
 
 		celestialBodies[0] = new Planet(128, 128, 128, 600, 450, 8, -4.7, 0, 9);
 		celestialBodies[1] = new Planet(207, 153, 52, 752, 400, 12, 0, 2.5, 900);
@@ -32,7 +30,6 @@ public class SystemPlanet extends JPanel {
 		celestialBodies[5] = new Planet(112, 128, 144, 600, -150, 15, 1.2, 0, 900);
 		celestialBodies[6] = new Planet(196, 233, 238, 600, -175, 15, 1.2, 0, 900);
 		celestialBodies[7] = new Planet(66, 98, 243, 0, 400, 13, 0, -1.2, 900);
-
 		celestialBodies[8] = new Planet(255, 140, 0, 600, 400, 30, .1, 0, 1000);
 
 		//setBackground(Color.BLACK);
@@ -46,6 +43,19 @@ public class SystemPlanet extends JPanel {
 		};
 
 		thread.start();
+	}
+
+	public void paintComponent(Graphics g) {
+
+		Graphics2D g2 = (Graphics2D) g;
+		int width2 = getWidth();
+		int height2 = getHeight();
+		System.out.println("width  " + width2 + " height  " + height2);
+		 
+		g2.drawImage(img, 0, 0, width2, height2, this);
+
+		for (Planet body : celestialBodies)
+			body.draw(g, size);
 	}
 
 	private void gameLoop() {
@@ -62,71 +72,6 @@ public class SystemPlanet extends JPanel {
 				Thread.sleep(DELAY);
 			} catch (InterruptedException ex) {
 			}
-		}
-	}
-
-	class Model extends JPanel implements KeyListener, MouseListener {
-		public Model() {
-
-			setFocusable(true);
-			requestFocus();
-			addKeyListener(this);
-			addMouseListener(this);
-		}
-
-		public void paintComponent(Graphics g) {
-
-			Graphics2D g2 = (Graphics2D) g;
-			g2.drawImage(img, 0, 0, getWidth(), getHeight(), this);
-
-			for (Planet body : celestialBodies)
-				body.draw(g, size);
-		}
-
-		public void keyTyped(KeyEvent e) {
-
-		}
-
-		public void mousePressed(MouseEvent e) {
-
-		}
-
-		public void mouseReleased(MouseEvent e) {
-
-		}
-
-		public void mouseEntered(MouseEvent e) {
-
-		}
-
-		public void mouseExited(MouseEvent e) {
-
-		}
-
-		public void mouseClicked(MouseEvent e) {
-
-		}
-
-		public void keyPressed(KeyEvent e) {
-
-		}
-
-		@Override
-		public void keyReleased(KeyEvent e) {
-
-			if (e.getKeyCode() == KeyEvent.VK_PLUS || e.getKeyCode() == KeyEvent.VK_EQUALS)
-				size += .1;
-
-			if (e.getKeyCode() == KeyEvent.VK_MINUS && size > 0)
-				size -= .1;
-
-			if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-				stop = !stop;
-			}
-			if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-				System.exit(0);
-			}
-
 		}
 
 	}
