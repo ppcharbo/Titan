41b68a775f80b189a8c509e376b91d7be1efa998
basaratojo <pierrepaul.charbonnier@gmail.com>
2021-03-17 11:31:03 +0100

    test case solver

---
+++ src/titan/tests/ODESolverTest.java
@@ -0,0 +1,24 @@
+package titan.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ODESolverTest {
+
+	@Test
+	public void testSolveODEFunctionInterfaceStateInterfaceDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSolveODEFunctionInterfaceStateInterfaceDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStep() {
+		fail("Not yet implemented");
+	}
+
+}
