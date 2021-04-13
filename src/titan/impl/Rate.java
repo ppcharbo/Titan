8d9ecb32efcf54fdee5f0ae9c22b21c1f3253404
Roman20022002 <72268723+Roman20022002@users.noreply.github.com>
2021-04-13 20:48:39 +0200

    Runge Kutta addition (new addRates method in Rate class)

--- src/titan/impl/Rate.java
+++ src/titan/impl/Rate.java
@@ -31,4 +31,22 @@ public class Rate implements RateInterface {
 
 		return speed;
 	}
+	
+	public static Rate addRates(Rate k1, Rate k2, Rate k3, Rate k4) {
+		
+		Vector3d p1 = k1.position();
+		Vector3d p2 = (Vector3d) k2.position().mul(2);
+		Vector3d p3 = (Vector3d) k3.position().mul(2);
+		Vector3d p4 = k4.position();
+		
+		Vector3d s1 = k1.speed();
+		Vector3d s2 = (Vector3d) k2.speed().mul(2);
+		Vector3d s3 = (Vector3d) k3.speed().mul(2);
+		Vector3d s4 = k4.speed();
+		
+		Vector3d ratePosition = (Vector3d) p1.add(p2).add(p3).add(p4);
+		Vector3d rateSpeed = (Vector3d) s1.add(s2).add(s3).add(s4);
+		
+		return new Rate(ratePosition, rateSpeed);
+	}
 }
