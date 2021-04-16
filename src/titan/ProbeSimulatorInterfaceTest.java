eaeba8c5b855991b656f93c2403af3ccdea56941
TheChickenAgent <m.elands@student.maastrichtuniversity.nl>
2021-04-16 18:52:20 +0200

    Error from Eclipse, still velocity remainder.

--- src/titan/ProbeSimulatorInterfaceTest.java
+++ src/titan/ProbeSimulatorInterfaceTest.java
@@ -12,7 +12,7 @@ public class ProbeSimulatorInterfaceTest {
 		ProbeSimulatorInterface probeSimulator = new ProbeSimulator();
 		
 		double[] ts = {100D,200D};
-		Vector3dInterface[] positions = probeSimulator.trajectory(Planet.SHIP.getPosition(), Planet.SHIP.getVelocity(),ts );
+		Vector3dInterface[] positions = probeSimulator.trajectory(Planet.SHIP.getPosition(), Planet.SHIP.getSpeed(),ts );
 		
 		for (Vector3dInterface vector3dInterface : positions) {
 			System.out.println("posistion "+vector3dInterface);
