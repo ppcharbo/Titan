c03f4f3d2699b2a4b31b283c7a583eb5bbba208d
Pierre Paul <pierrepaul.charbonnier@gmail.com>
2021-04-26 14:09:50 +0200

    solver fix

--- src/titan/impl/ODESolverRungeKutta.java
+++ src/titan/impl/ODESolverRungeKutta.java
@@ -6,50 +6,49 @@ import titan.StateInterface;
 
 public class ODESolverRungeKutta implements ODESolverInterface {
 
-		@Override
-		public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double[] ts) {
-			
-			StateInterface[] arr = new StateInterface[ts.length];
-			arr[0] = y0;
-			double stepSize;
-			
-			for (int i = 1; i < arr.length; i++) {
-				
-				stepSize = ts[i] - ts[i - 1];
-				arr[i] = step(f, ts[i], arr[i - 1], stepSize);
-			}
-			return arr;
-		}
+	@Override
+	public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double[] ts) {
 
-		@Override
-		public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double tf, double h) {
-			
-			StateInterface[] arr = new StateInterface[(int) Math.round((tf / h) + 1)];
-			arr[0] = y0;
-			double stepSize = h;
-			double currentTime = 0;
-			
-			for (int i = 1; i < arr.length; i++) {
-				
-				if (i == arr.length - 1) {
-					// we are in last step and have to check our remaining step size
-					stepSize = tf - currentTime;
-				}
-				currentTime += stepSize;
-				arr[i] = step(f, currentTime, arr[i - 1], stepSize);
-			}
-			return arr;
-		}
+		StateInterface[] arr = new StateInterface[ts.length];
+		arr[0] = y0;
 
-		@Override
-		public StateInterface step(ODEFunctionInterface f, double t, StateInterface y, double h) {
-			
-			Rate k1 = (Rate) f.call(t, y);
-			Rate k2 = (Rate) f.call(t+h/2, y.addMul(h/2, k1));
-			Rate k3 = (Rate) f.call(t+h/2, y.addMul(h/2, k2));
-			Rate k4 = (Rate) f.call(t+h, y.addMul(h, k3));
-			
-			return y.addMul(h/6, Rate.addRates(k1, k2, k3, k4));
+		for (int i = 1; i < arr.length; i++) {
+
+			double stepSize = ts[i] - ts[i - 1];
+			arr[i] = step(f, ts[i-1], arr[i - 1], stepSize);
 		}
+		return arr;
 	}
 
+	@Override
+	public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double tf, double h) {
+
+		StateInterface[] arr = new StateInterface[(int) Math.round((tf / h) + 1)];
+		arr[0] = y0;
+		double stepSize = h;
+		double currentTime = 0;
+
+		for (int i = 1; i < arr.length; i++) {
+
+			if (i == arr.length - 1) {
+				// we are in last step and have to check our remaining step size
+				stepSize = tf - currentTime;
+			}
+// swich the two lines
+			arr[i] = step(f, currentTime, arr[i - 1], stepSize);
+			currentTime += stepSize;
+		}
+		return arr;
+	}
+
+	@Override
+	public StateInterface step(ODEFunctionInterface f, double t, StateInterface y, double h) {
+
+		Rate k1 = (Rate) f.call(t, y);
+		Rate k2 = (Rate) f.call(t + h / 2, y.addMul(h / 2, k1));
+		Rate k3 = (Rate) f.call(t + h / 2, y.addMul(h / 2, k2));
+		Rate k4 = (Rate) f.call(t + h, y.addMul(h, k3));
+
+		return y.addMul(h / 6, Rate.addRates(k1, k2, k3, k4));
+	}
+}
