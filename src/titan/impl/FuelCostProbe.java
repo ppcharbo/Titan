f40b7cb63b2fd7684c05fd9d4bc54fbfc40e5453
Chen <2547338075@qq.com>
2021-05-04 18:47:25 +0800

    Create FuelCostProbe.java
    to calculate expelled of fuel mass flow rate

---
+++ src/titan/impl/FuelCostProbe.java
@@ -0,0 +1,26 @@
+package titan.impl;
+
+/*
+this class is a function about costing fuel when the probe moving and especially when Probe approch the surface of the moon
+using the energy conservation theorem 
+
+Vairable:
+M: the rocket mass
+m: mass of expell fuel
+Ve:relative velocity
+V0:initial velocity of rocket
+Vr:final Velocity of the rocket
+
+initial momentum=Final momentum
+mass*velocity is momentum
+(M+m)*V0=M*Vr+(V0-Ve)m
+
+M(Vr-V0)/▲t=Ve*▲m/▲t
+
+Thrust=F=Ma=Ve*(m#) m#:expelled of fuel mass flow rate.
+
+*/
+
+public class FuelCostProbe {
+    
+}
