630a6e98eef4aa120656c1c9496b3b9059b706a9
TheChickenAgent <m.elands@student.maastrichtuniversity.nl>
2021-05-12 22:39:58 +0200

    Adjustments solver for trajectory

--- src/titan/impl/ODESolver.java
+++ src/titan/impl/ODESolver.java
@@ -4,9 +4,21 @@ import titan.ODEFunctionInterface;
 import titan.ODESolverInterface;
 import titan.StateInterface;
 
-
+/*
+ * A class for solving a general differential equation dy/dt = f(t,y)
+ *     y(t) describes the state of the system at time t
+ *     f(t,y(t)) defines the derivative of y(t) with respect to time t
+ */
 public class ODESolver implements ODESolverInterface {
-
+	
+	/*
+	 * Solve the differential equation by taking multiple steps.
+	 *
+	 * @param   f       the function defining the differential equation dy/dt=f(t,y)
+	 * @param   y0      the starting state
+	 * @param   ts      the times at which the states should be output, with ts[0] being the initial time
+	 * @return  an array of size ts.length with all intermediate states along the path
+	 */
 	@Override
 	public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double[] ts) {
 		StateInterface[] arr = new StateInterface[ts.length];
@@ -20,50 +32,57 @@ public class ODESolver implements ODESolverInterface {
 		return arr;
 	}
 
+	/*
+	 * Solve the differential equation by taking multiple steps of equal size, starting at time 0.
+	 * The final step may have a smaller size, if the step-size does not exactly divide the solution time range
+	 *
+	 * @param   f       the function defining the differential equation dy/dt=f(t,y)
+	 * @param   y0      the starting state
+	 * @param   tf      the final time
+	 * @param   h       the size of step to be taken
+	 * @return  an array of size round(tf/h)+1 including all intermediate states along the path
+	 */
+	@Override
 	public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double tf, double h) {
 		
-		StateInterface[] arr = new StateInterface[(int) Math.round((tf / h) + 1)];
+		StateInterface[] arr = new StateInterface[(int) Math.ceil((tf / h) + 1)];
 		int i = 0;
 		arr[i] = y0;
-		i = 1;
+		((State) y0).setTime(0);
 		
-		
-		// double stepSize = h;
 		double currentTime = 0;
-		
-		
-		// w(i+1) = w(i) + h*f(t,y)
-		// y_next = y_current + h*v
-		
+
 		while(currentTime < tf) {
-			// operations
+			// operations:
 			// w(i+1) = w(i) + h*f(t,y)
-			arr[i] = step(f, currentTime, arr[i-1], h);
-			if(arr[i] == null)
-			//i++;
-			currentTime = currentTime + h; // update time 
+			// y_next = y_current + h*v
+			// w(i+1) = w(i) + h*f(t,y)
+			arr[i+1] = step(f, currentTime, arr[i], h); // calculate the next step
+			//((State) arr[i+1]).setTime(currentTime+h); // set time for the next step
+			System.out.println("i: " + (i+1) + " for tf: " + (tf/(24 * 60 * 60) + " day(s)")); // display for debugging
+			i = i + 1; // update array position
+			currentTime = currentTime + h; // update time
 		}
 		
-		/*
-		for (int i = 1; i < arr.length; i++) {
-			
-			if (i == arr.length - 1) {
-				// we are in last step and have to check our remaining step size
-				stepSize = Math.IEEEremainder(tf, h);
-			}
-			arr[i] = step(f, currentTime, arr[i - 1], stepSize);
-			currentTime += stepSize;
-		}
-		*/
+		// checking for null problems
 		for (int a = 0; a < arr.length; a++) {
 			if(arr[a] == null) {
-				throw new NullPointerException("Some state is null: " + a);
+				throw new NullPointerException("Some state is null in the ODESolver, state= " + a);
 			}
 		}
 		return arr;
 		
 	}
 
+	/*
+	 * Update rule for one step.
+	 *
+	 * @param   f   the function defining the differential equation dy/dt=f(t,y)
+	 * @param   t   the time
+	 * @param   y   the state
+	 * @param   h   the step size
+	 * @return  the new state after taking one step
+	 */
 	@Override
 	public StateInterface step(ODEFunctionInterface f, double t, StateInterface y, double h) {
 
