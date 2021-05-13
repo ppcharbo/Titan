e2985d13f47620c0c92480f4c614daa2fa681905
TheChickenAgent <m.elands@student.maastrichtuniversity.nl>
2021-05-13 11:05:19 +0200

    Bug trace

--- src/titan/impl/ODEFunction.java
+++ src/titan/impl/ODEFunction.java
@@ -38,6 +38,7 @@ public class ODEFunction implements ODEFunctionInterface {
 	 */
 	public Vector3d[] gravitationalForce(State y) {
 
+		System.out.println("Casting of state: " + y.getPosition()[0].getX());
 		Vector3d[] allForces = new Vector3d[numberOfPlanet];
 		// Calculate force of all the surrounding planets except that same planet
 		for (int i = 0; i < numberOfPlanet; i++) {
@@ -48,21 +49,36 @@ public class ODEFunction implements ODEFunctionInterface {
 				// Do not consider the current planet
 				if (i != j) {
 
+					// BUG is already here
 					Vector3dInterface xi = y.getPosition()[i];
+					//System.out.println("Vector xi creation: " + y.getPosition()[i].getX());
 					Vector3dInterface xj = y.getPosition()[j];
 
 					// Vector from our planet towards the other planet (attractive force)
 					Vector3dInterface nTop = xj.sub(xi);
 
 					// Vector from our planet towards the other planet (attractive force)
+					// BUG is already here:
+					// System.out.println("Xi: " + xi.getX());
 					Vector3dInterface nBottom = xi.sub(xj);
 
+					// BUG is NOT in GMM
 					double GMM = G * AllPlanet.getListOfPlanets().get(i).getMass()
 							* AllPlanet.getListOfPlanets().get(j).getMass();
+					
+					// BUG is in this
+					//System.out.println("Bottom X: " + nBottom.getX());
 					double GMMdivNorm = GMM / Math.pow(nBottom.norm(), 3);
-
+					// System.out.println(GMMdivNorm);
+					
+					
 					// Add contribution planet p at each iteration
+					// BUG (IS PROBABLY) here
+					// System.out.println("Inside Fg method: " + force.getX());
 					force = (Vector3d) force.addMul(GMMdivNorm, nTop);
+					
+					// BUG (IS PROBABLY) here
+					// System.out.println("Inside Fg method: " + force.getX());
 
 				}
 			}
@@ -78,13 +94,16 @@ public class ODEFunction implements ODEFunctionInterface {
 	public Vector3d[] accelerationForce(State y) {
 		
 		Vector3d[] forces = gravitationalForce(y);
+		
+		// BUG (IS PROBABLY) here
+		// System.out.println("Grav force: " + forces[0].getX());
 		Vector3d[] acceleration = new Vector3d[numberOfPlanet];
 		for (int i = 0; i < numberOfPlanet; i++) {
 			acceleration[i] = (Vector3d) forces[i].mul(1 / (AllPlanet.getListOfPlanets().get(i).getMass()));
 		}
 		
-		//BUG (IS PROBABLY) here
-		System.out.println("Acc" + acceleration[0].getX());
+		// BUG (IS PROBABLY) here
+		// System.out.println("Acc" + acceleration[0].getX());
 		return acceleration;
 
 	}
