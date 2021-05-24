1a94b2ca7e685da0db42ca2dda0f22f172ddda99
Roman ILIC <romanilic2002@gmail.com>
2021-05-24 19:17:13 +0200

    ODEFunctionShip

--- src/titan/impl/ShipFuelCosts.java
+++ src/titan/impl/ShipFuelCosts.java
@@ -3,39 +3,41 @@ package titan.impl;
 import titan.RateInterface;
 import titan.Vector3dInterface;
 
-//public class FuelCostProbe implements ProbeSimulatorInterface{
 public class ShipFuelCosts {
 
-	private static final double MASS_THIRSTY_CRAFT = 7.8e4 + 6e3; // craft + lander
-	private static final double MASS_FUEL=0.08e4; // assumption
+	private static final double MASS_THIRSTY_CRAFT = 7.8e4 + 6.0e3; // craft + lander
+	private static final double MASS_FUEL = 0.08e4; // assumption
+	private static final double MAX_THRUST = 3e7; // Newton
+	private static final double EFFECTIVE_EXHAUST_VELOCITY_COMBUSTION = 4e3; // m/s
 	
-	private static double maxthrusty;
-	private static double exit_velocity;
-	private static State Maxthrust;
 	private static State Post_velocity;
 	private static State Current_velocity;
 	private static double mass_flow_rate;
 	private static double engineTime;
 	private static double fuelCost;
 
+	// inclination = velocity vector
+	
 	public ShipFuelCosts() {
+		
 	}
 
 	public static double massFlowrate() {
 
-		return maxthrusty / exit_velocity;
+		return MAX_THRUST / EFFECTIVE_EXHAUST_VELOCITY_COMBUSTION;
 		//flowrate is also a constant 
 	}
 
 	// Fmax /   total   mass  -  massflowrate  *  enginetime could be simplify as 1/(1-t) to
 	// Derivative, equal -1/(1-t)^2
 
-	public static StateInterface acceleration() {
-		return Maxthrust.getAcceleration / (MASS_THIRSTY_CRAFT + MASS_FUEL);
+	public static Vector3d acceleration(double t) {
+		
+		return null;//MAX_THRUST.getAcceleration() / (MASS_THIRSTY_CRAFT + MASS_FUEL);
 	}
 
 	public static double engineTime() {
-		return ((MASS_THIRSTY_CRAFT + MASS_FUEL)-Math.sqrt((-1 * Maxthrust.getAcceleration)/ velocity())) / mass_flow_rate;
+		return ((MASS_THIRSTY_CRAFT + MASS_FUEL)-Math.sqrt((-1 * MAX_THRUST.getAcceleration)/ velocity())) / mass_flow_rate;
 		//use v(t) to get t(v) function, so as we know the delta(v), we could get delta(t)
 	}
 
