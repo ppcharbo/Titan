9289562f44f31af2f5af8c866260136419fdc22a
Chen <2547338075@qq.com>
2021-05-25 15:46:13 +0800

    Update FuelCostProbe.java
    Finished the test of Fuelcost

--- src/titan/impl/FuelCostProbe.java
+++ src/titan/impl/FuelCostProbe.java
@@ -1,53 +1,114 @@
 package titan.impl;
 
-/*
-
-*/
-
 //public class FuelCostProbe implements ProbeSimulatorInterface{
 public class FuelCostProbe {
 
-	private static double mass_craft;
-	private static double mass_lander;
-	private static double mass_totalfuel; // have to calculate by using exit velocity and distance from earth to titan.
-	private static double exit_velocity;
-	private static double maxthrust;
-	private static double mass_flow_rate;
-	private static double acceleration;
-	private static double engineTime;
-	private static double fuelCost;
+	private static final double MASS_EMPTY_CRAFT = 7.8e4;
+	private static final double MASS_totalfuel=7.8e4; // assume.
+	private static double Current_mass_fuel;
 
-	public FuelCostProbe() {
-		mass_craft = 7.8e4;
-		mass_lander = 6e3;
-		exit_velocity = 2e4;
-		maxthrust = 3e7;
+	//max thrust= 30 mN= 30000000 N=3e7 N
+	private static double maxthrusty=3e7;//N
+
+	private static double effective_exhaust_velocity=2e4;//m/s
+
+	//Velocity
+	private  Vector3d delta_velocity;
+	private  Vector3d Post_velocity;
+	private  Vector3d Current_velocity;
+	private  Vector3d acceleration;
+
+	private static double mass_flow_rate;
+	private static double engineTime; //fuel burning time
+	private static double fuelCost;
+	private static double accleration_value; //the value of acceleration when engine turn on,  using accleration.norm()
+
+	public FuelCostProbe(Vector3d current_velocity, Vector3d post_velocity) {//constructor
+		this.Current_velocity=current_velocity;
+		this.Post_velocity=post_velocity;
 	}
 
 	public static double massFlowrate() {
 
-		return maxthrust / exit_velocity;
+		mass_flow_rate=maxthrusty / effective_exhaust_velocity;
+
+		return mass_flow_rate;
+		//flowrate is also a constant 
 
 	}
 
-	// Fmax /   total   mass  -  massflowrate  *  enginetime could be simplify as 1/(1-t) to
-	// Derivative, equal -1/(1-t)^2
+	public Vector3d changeVelocity(){
 
-	public static double acceleration() {
-		return maxthrust / (mass_craft + mass_lander + mass_totalfuel);
+		//delta_velocity between engine on.
+		delta_velocity=(Vector3d) Post_velocity.sub(Current_velocity);
+
+		return delta_velocity;
 	}
 
-	public static double engineTime() {
-		return (Math.sqrt((-1 * maxthrust) / velocity()) - mass_craft + mass_lander + mass_totalfuel) / mass_flow_rate;
-	}
+	
+	 /**
+	* first :calculate acceleration of the probe
+	 * using M*a=Ve*mass flow rate
+	 * M=Rocket mass
+	 * Ve=exhuast velocity
+	 * 
+	 * a=Ve*mass flow rate/M
+	 
+	* Second: calculate engine time 
+	  t=(V(t)-V(0))/a
+	  t=delta(V)/a
+	*/
 
-	public static double velocity() {
-		return -maxthrust / (mass_craft + mass_lander + mass_totalfuel - mass_flow_rate * engineTime);
-	}
+	  public double calcTime(){
 
-	public static double fuelcost() {
-		fuelCost = mass_flow_rate * engineTime;
-		return (Double) null;
+		accleration_value=effective_exhaust_velocity*massFlowrate()/MASS_EMPTY_CRAFT;
+
+		engineTime=(changeVelocity().norm())/accleration_value;
+
+		return engineTime;
+	  }
+
+	  /**
+	   * calculate direction of accleration using Vector3d.
+	   */
+
+	  public Vector3d calAce(){
+
+		  acceleration=(Vector3d)changeVelocity().mul(1/calcTime());
+
+		  return acceleration;
+	  }
+
+	  public void Fuelcost(){
+		  changeVelocity();
+		  calcTime();
+
+		  fuelCost=engineTime*mass_flow_rate;
+
+		  Current_mass_fuel=MASS_totalfuel-fuelCost;
+
+		  if(Current_mass_fuel<0){
+			  System.out.println("NO FUEL LEFT,PLEASE TRY AGAIN");	
+		  }
+
+	  }
+
+	  public double getFuelCost(){
+		  return fuelCost;
+	  }
+
+	  
+	  public static void main(String[] args){
+		Vector3d v0=new Vector3d(1,1,1);
+		Vector3d vt=new Vector3d(1000,1000,1000);
+
+		FuelCostProbe f=new FuelCostProbe(v0,vt);
+		f.Fuelcost();
+		double fuelCost=f.getFuelCost();
+		Vector3d accleration=f.calAce();
+
+		System.out.println("Fuelcost: "+fuelCost+" kg");
+		System.out.println("The accleration after thrust is :"+ accleration);
 	}
 
 }
