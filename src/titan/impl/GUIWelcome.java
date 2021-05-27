ec7a7e53be29470b79f5d311cb2f04b5a6e478e2
TheChickenAgent <m.elands@student.maastrichtuniversity.nl>
2021-05-27 14:53:00 +0200

    Removed unused imports

--- src/titan/impl/GUIWelcome.java
+++ src/titan/impl/GUIWelcome.java
@@ -5,8 +5,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
