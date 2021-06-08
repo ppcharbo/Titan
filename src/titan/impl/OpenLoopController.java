97ce342dddc0d6b6f1e3cd58345c5a5b0cce6800
TheChickenAgent <m.elands@student.maastrichtuniversity.nl>
2021-06-08 21:56:43 +0200

    OpenLoopController and InputFunction update

--- src/titan/impl/OpenLoopController.java
+++ src/titan/impl/OpenLoopController.java
@@ -5,9 +5,40 @@ public class OpenLoopController {
 	public InputFunctionLanding u;
 	public InputFunctionLanding v;
 	
-	public InputFunctionLanding OpenLoopLandingSimulator(Vector3d[] Xposition, Vector3d[] Yposition) {
+	public InputFunctionLanding OpenLoopLandingSimulatorWRTU(State initialLaunchState) {
 		
+		//TODO Change shipWhenDistanceIsSmallest to ship
+		int shipWhenDistanceIsSmallest = 0;
+		Vector3d positionLaunch = initialLaunchState.getPosition()[shipWhenDistanceIsSmallest];
+		Vector3d velocityLaunch = initialLaunchState.getVelocity()[shipWhenDistanceIsSmallest];
+		double x = positionLaunch.getX();
+		double y = positionLaunch.getY();
+		double vx = velocityLaunch.getX();
+		double vy = velocityLaunch.getY();
+		
+		//TODO define eta's
+		double eta = 0;
+		double eta2 = 0;
+		InputFunctionLanding f = new InputFunctionLanding(x, y, eta, vx, vy, eta2);
 		return u;
+	}
+	
+	public InputFunctionLanding OpenLoopLandingSimulatorWRTV(State initialLaunchState) {
+		//TODO Change shipWhenDistanceIsSmallest to ship
+		int shipWhenDistanceIsSmallest = 0;
+		Vector3d positionLaunch = initialLaunchState.getPosition()[shipWhenDistanceIsSmallest];
+		Vector3d velocityLaunch = initialLaunchState.getVelocity()[shipWhenDistanceIsSmallest];
+		double x = positionLaunch.getX();
+		double y = positionLaunch.getY();
+		double vx = velocityLaunch.getX();
+		double vy = velocityLaunch.getY();
+		
+		//TODO define eta's
+		double eta = 0;
+		double eta2 = 0;
+		InputFunctionLanding f = new InputFunctionLanding(x, y, eta, vx, vy, eta2);
+		
+		return v;
 	}
 	
 }
