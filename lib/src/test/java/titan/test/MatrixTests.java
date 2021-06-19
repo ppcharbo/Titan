017ab198d16859a2af91a83bbb11c30d1cd89b4e
TheChickenAgent <m.elands@student.maastrichtuniversity.nl>
2021-06-19 17:21:14 +0200

    Removed test case 6 by 6

--- lib/src/test/java/titan/test/MatrixTests.java
+++ lib/src/test/java/titan/test/MatrixTests.java
@@ -27,25 +27,6 @@ public class MatrixTests {
 		}
 	}
 	
-	@Test
-	public void testInverse6By6() {
-
-		//Don't know how to make sure that we can generate a 6by6 invertible matrix
-		/*
-		ODESolverNewtonRaphson solver = new ODESolverNewtonRaphson();
-		double[][] A = { { 9,5,2 }, {4,3,6}, {5,10,16} };
-		double[][] inverse = solver.inverseMatrix6by6(A);
-		double[][] inverseMatlab = { {0.05263157894736846, 0.26315789473684204, -0.10526315789473685},
-									 {0.14912280701754377, -0.5877192982456139, 0.2017543859649123},
-									 {-0.10964912280701754, 0.2850877192982456, -0.030701754385964914} };
-		
-		//use a double for loop to check every element in the inversed matrix
-		for(int i = 0; i < inverse.length; i++) {
-			for (int j = 0; j < inverse[0].length; j++) {
-				assertEquals(inverseMatlab[i][j], inverse[i][j], ACCURACY);
-			}
-		}*/
-	}
 	
 	@Test
 	public void testMultiplication(){
@@ -59,7 +40,5 @@ public class MatrixTests {
 		for (int i = 0; i < resultMatlab.length; i++) {
 			assertEquals(resultMatlab[i], multiplicationAv[i], ACCURACY);
 		}
-	}
-	
+	}	
 }
-
