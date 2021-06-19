beb6a7d1131d9d58137361db4245ed9274bd06a5
Roman20022002 <72268723+Roman20022002@users.noreply.github.com>
2021-06-19 17:27:22 +0200

    Add files via upload

--- lib/src/test/java/titan/test/MatrixTests.java
+++ lib/src/test/java/titan/test/MatrixTests.java
@@ -7,38 +7,99 @@ import titan.impl.ODESolverNewtonRaphson;
 
 public class MatrixTests {
 	
-	public final double ACCURACY = 1E-03;
+	public final double ACCURACY = 1E-3;
 	
+<<<<<<< HEAD
 	@Test
-	public void testInverse3By3() {
-
-		ODESolverNewtonRaphson solver = new ODESolverNewtonRaphson();
-		double[][] A = { { 9,5,2 }, {4,3,6}, {5,10,16} };
-		double[][] inverse = solver.inverseMatrix3by3(A);
-		double[][] inverseMatlab = { {0.05263157894736846, 0.26315789473684204, -0.10526315789473685},
-									 {0.14912280701754377, -0.5877192982456139, 0.2017543859649123},
-									 {-0.10964912280701754, 0.2850877192982456, -0.030701754385964914} };
+	public void testInverse6By6() {
 		
-		//use a double for loop to check every element in the inversed matrix
-		for(int i = 0; i < inverse.length; i++) {
-			for (int j = 0; j < inverse[0].length; j++) {
-				assertEquals(inverseMatlab[i][j], inverse[i][j], ACCURACY);
+		double[][] identityMatrix = new double[6][6];
+		
+		for (int i=0; i<identityMatrix.length; i++) {
+			for (int j=0; j<identityMatrix[0].length; j++) {
+				if (i==j) {
+					identityMatrix[i][j] = 1;
+				}
+				else {
+					identityMatrix[i][j] = 0;
+				}
+			}
+		}
+		
+		double[][] inversedIdentityMatrix = ODESolverNewtonRaphson.inverse6x6JacobianMatrix(identityMatrix);
+		for (int i=0; i<identityMatrix.length; i++) {
+			for (int j=0; j<identityMatrix[0].length; j++) {
+				System.out.println(inversedIdentityMatrix[i][j]);
+				assertEquals(identityMatrix[i][j], inversedIdentityMatrix[i][j], ACCURACY);
 			}
 		}
 	}
+
+	/*
+	@Test
+	//Don't know how to make sure that we can generate a 6by6 invertible matrix
+	ODESolverNewtonRaphson solver = new ODESolverNewtonRaphson();
+	double[][] A = { {1, 0, 0, 0, 0, 0}, {};
+	double[][] inverse = solver.inverse(A);
+	double[][] inverseMatlab = { {0.05263157894736846, 0.26315789473684204, -0.10526315789473685},
+								 {0.14912280701754377, -0.5877192982456139, 0.2017543859649123},
+								 {-0.10964912280701754, 0.2850877192982456, -0.030701754385964914} };
 	
+	//use a double for loop to check every element in the inversed matrix
+	for(int i = 0; i < inverse.length; i++) {
+		for (int j = 0; j < inverse[0].length; j++) {
+			assertEquals(inverseMatlab[i][j], inverse[i][j], ACCURACY);
+		}
+	}
+	*/
+=======
+>>>>>>> branch 'main' of https://github.com/Basaratojo/Titan.git
 	
 	@Test
-	public void testMultiplication(){
-		ODESolverNewtonRaphson solver = new ODESolverNewtonRaphson();
+	public void testMultiplication1() {
+		
 		double[][] A = { {1, 18}, {2, 7} };
 		double[] v = { 5, 6 };
 		
-		double[] multiplicationAv = solver.matrixMultiplication(A, v);
+		double[] multiplicationAv = ODESolverNewtonRaphson.matrixMultiplication(A, v);
 		double[] resultMatlab = { 113, 52 };
 		
 		for (int i = 0; i < resultMatlab.length; i++) {
 			assertEquals(resultMatlab[i], multiplicationAv[i], ACCURACY);
 		}
+<<<<<<< HEAD
+	}
+	
+	@Test
+	public void testMultiplication() {
+		double[][] identityMatrix1 = new double[6][6];
+		double[] identityMatrix2 = new double[6];
+		
+		for (int i=0; i<identityMatrix1.length; i++) {
+			for (int j=0; j<identityMatrix1[0].length; j++) {
+				if (i==j) {
+					identityMatrix1[i][j] = 1;
+				}
+				else {
+					identityMatrix1[i][j] = 0;
+				}
+			}
+		}
+		
+		for (int i=0; i<identityMatrix2.length; i++) {
+					identityMatrix2[i] = 1;
+		}
+		
+		double[] multiplication = ODESolverNewtonRaphson.matrixMultiplication(identityMatrix1, identityMatrix2);
+		
+		
+		for (int i=0; i < multiplication.length; i++) {
+			
+			assertEquals(1, multiplication[i], ACCURACY);
+		}
+	}
+}
+=======
 	}	
 }
+>>>>>>> branch 'main' of https://github.com/Basaratojo/Titan.git
